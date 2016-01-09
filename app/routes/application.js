4c1df5cf77d0e983a1bf59470394657d38f9f534
Matic Jurglic <matic@jurglic.si>
2016-01-09 16:30:25 +0100

    user model, load user after successful auth

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+
+  sessionAuthenticated: function() {
+    this._super(...arguments); //for keeping the defaults (attempted transition, routeAfterAuth config etc..)
+    this.loadUser();
+  },
+
+  loadUser: function() {
+    this.store.findRecord('user', 'me')
+  },
+
   actions: {
     logout() {
       this.get('session').invalidate();
