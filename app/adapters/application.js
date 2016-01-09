5bee884a92d710de1b4ed75ea5fc1413aff96eef
Matic Jurglic <matic@jurglic.si>
2016-01-09 16:20:18 +0100

    setup adapter with oauth autorizer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:oauth2'
+  authorizer: 'authorizer:application'
 });
