abf46f586eae2597bfdf9012994f0b6c2b961c5a
Matic Jurglic <matic@jurglic.si>
2016-01-09 15:05:53 +0100

    don't need this action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
-  actions: {
-    logout() {
-      this.get('session').invalidate()
-    }
-  }
+  session: Ember.inject.service('session')
 });
