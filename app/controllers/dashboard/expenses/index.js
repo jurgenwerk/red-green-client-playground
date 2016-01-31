1575a006bd0f1bb0d60a12caec839aaedb151dbc
Matic Jurglic <matic@jurglic.si>
2016-01-31 21:35:32 +0100

    implement deleting

--- app/controllers/dashboard/expenses/index.js
+++ app/controllers/dashboard/expenses/index.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-client/mixins/balance-change-properties';
 
 export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
-
+  actions: {
+    delete: function(balanceChange) {
+      if (confirm("Are you sure?")) {
+        balanceChange.destroyRecord().then(() => this.send('refreshRoute'));
+      }
+    }
+  }
 });
