ce1e955148473e4e37e754846799ba014cd3ea42
Matic Jurglic <matic@jurglic.si>
2016-02-08 19:54:26 +0100

    get is preferred

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     login() {
-      const username = this.username;
-      const password = this.password;
+      const username = this.get('username');
+      const password = this.get('password');
       this.get('session').authenticate('authenticator:oauth2', username, password)
         .catch(() => { this.set('errorMessage', "Invalid login."); }
       );
