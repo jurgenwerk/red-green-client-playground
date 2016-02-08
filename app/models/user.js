093bee18c6c3ddc30f43ddeec66fddc1e46feaaf
Matic Jurglic <matic@jurglic.si>
2016-02-08 19:55:49 +0100

    remove readonly

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
   currenciesData: Ember.inject.service(),
   email: DS.attr('string'),
   currency: DS.attr('string'),
-  password: DS.attr('string', { readOnly: true }),
+  password: DS.attr('string'),
   currencySymbol: Ember.computed('currency', function() {
     return this.get('currenciesData.currencies').findBy('cc', this.get('currency')).symbol;
   })
