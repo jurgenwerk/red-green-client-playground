c1936141383458b0ac320da84a32726645cac032
Matic Jurglic <matic@jurglic.si>
2016-02-08 19:54:13 +0100

    move signup logic to route

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.store.createRecord('user');
+  },
+  actions: {
+    signup: function() {
+      let user = this.controller.get('model');
+      const email = user.get('email');
+      const password = user.get('password');
+      user.set('currency', 'USD');
+      user.save().then((user) => {
+        this.get('session').authenticate('authenticator:oauth2', email, password);
+        user.set('password', null);
+      }).finally(() => { user.set('password', null); });
+    }
   }
 });
