1b6f92fd8868de0022cd4ac647fcee8daeb54e06
Matic Jurglic <matic@jurglic.si>
2016-02-27 17:10:18 +0100

    get rid of login controller

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  actions: {
+    login(username, password) {
+      this.get('session').authenticate('authenticator:oauth2', username, password)
+        .catch(() => { this.set('errorMessage', "Invalid login."); }
+      );
+    }
+  }
+});
