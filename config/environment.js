ad5f11aaec8e746295fadf6c848e22a836751e9c
Matic Jurglic <matic@jurglic.si>
2016-02-27 16:09:51 +0100

    don't need this

--- config/environment.js
+++ config/environment.js
@@ -29,10 +29,6 @@ module.exports = function(environment) {
 
     ENV.serverURL = 'http://localhost:3000';
     ENV.apiBaseURL = ENV.serverURL + '/' + ENV.apiNamespace;
-
-    ENV['ember-simple-auth'] = {
-      baseURL: 'http://localhost:3000'
-    };
   }
 
   if (environment === 'test') {
